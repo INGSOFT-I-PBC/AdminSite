3e8d6fb1020358f0f1f1aefd5ecf32edd0aa3b9d
Luis Andrés Bajaña F <labfernandez2014@gmail.com>
2022-07-24 20:59:42 -0500

    Changed the core permission group strategy (#5)
    * Change the permission assignment
    
    * Updated models with new Codebase

---
+++ api/models/common.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class TimestampModel(models.Model):
+    """TimestampModel
+
+    This abstract model hold the data from a model that holds
+    the log when the 'state' change.
+    """
+
+    created_at = models.DateTimeField(null=False, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
+    deleted_at = models.DateTimeField(null=True, default=None)
+
+    class Meta:
+        abstract = True
+
+
+class TraceableModel(models.Model):
+    """TraceableModel
+
+    This models contains the registry of the user that change its
+    state.
+    """
+
+    created_by = models.ForeignKey(
+        "User", db_column="created_by", null=True, on_delete=models.RESTRICT, related_name="creator"
+    )
+    updated_by = models.ForeignKey(
+        "User",
+        db_column="updated_by",
+        null=True,
+        default=None,
+        on_delete=models.RESTRICT,
+        related_name="updater",
+    )
+    deleted_by = models.ForeignKey(
+        "User",
+        db_column="deleted_by",
+        null=False,
+        default=None,
+        on_delete=models.RESTRICT,
+        related_name="remover",
+    )
+
+    class Meta:
+        abstract = True
