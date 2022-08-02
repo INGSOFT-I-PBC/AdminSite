dbf7d1bd56580fc2b42742da209de2cc1ab28989
neoterux <labfernandez2014@gmail.com>
2022-08-01 21:07:23 -0500

    Removed leaked imports and fix lint

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
     import TheWelcome from '@/components/TheWelcome.vue'
-    import EButton from '@/components/custom/EButton.vue'
 </script>
 
 <template>
     <main>
         <TheWelcome />
-        <h1 class="text-on-primary">Hola</h1>
-        <EButton type="primary">Botón</EButton>
     </main>
 </template>
