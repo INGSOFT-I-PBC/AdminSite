3fd3494c2dd240220a779b0df0b965967ad7fba9
neoterux <labfernandez2014@gmail.com>
2022-08-01 20:26:19 -0500

    Formatted with new rules

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-    content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: [
+        './src/index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#F9A825',
+                    light: '#FFC107',
+                    dark: '#FF8F00',
+                },
+                on: {
+                    primary: {
+                        DEFAULT: '#424242',
+                        soft: '',
+                        hard: '',
+                        highlight: '#FFD54F',
+                    },
+                    secondary: {
+                        DEFAULT: '#E0E0E0',
+                        soft: '#BDBDBD',
+                        hard: '#FAFAFA',
+                    },
+                },
+                secondary: {
+                    DEFAULT: '#455A64',
+                    light: '#607D8B',
+                    dark: '#263238',
+                },
+            },
+        },
     },
     plugins: [],
 }
