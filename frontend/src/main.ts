4689d8af3340ef6c0e1c65a160f48a22a77061a2
neoterux <labfernandez2014@gmail.com>
2022-08-03 08:13:09 -0500

    Added Toast support to app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,29 @@
 import 'vite/modulepreload-polyfill'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Toast, {
+    type PluginOptions,
+    POSITION,
+} from 'vue-toastification'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
 import './index.css'
+import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 
+const defToastOptions: PluginOptions = {
+    position: POSITION.TOP_RIGHT,
+    closeOnClick: true,
+    pauseOnHover: true,
+    maxToasts: 5,
+}
+
 app.use(createPinia())
 app.use(router)
+app.use(Toast, defToastOptions)
 
 app.mount('#app')
