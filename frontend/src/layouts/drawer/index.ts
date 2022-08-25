2c61aaf0741a0b7eed6f0caec53b32770f13595f
PamelaRugel <parudiaz@espol.edu.ec>
2022-08-25 15:28:36 -0500

    cambios en menu

--- frontend/src/layouts/drawer/index.ts
+++ frontend/src/layouts/drawer/index.ts
@@ -1,9 +1,33 @@
 export const menus: MenuItem[] = [
+
     {
         label: 'Principal',
         path: '/home',
         icon: 'home',
         forceRender: true,
+    },
+    {
+        label: 'Dashboard',
+        path: '/dashboard',
+        icon: 'bar-chart-2',
+        forceRender: true,
+    },
+    {
+        label: 'Usuarios',
+        icon: 'users',
+        children: [
+            {
+                label: 'Empleados',
+                path: '/employee',
+
+            },
+            {
+                label: 'Clientes',
+                path: '/',
+
+            },
+
+        ],
     },
     {
         label: 'Administración',
