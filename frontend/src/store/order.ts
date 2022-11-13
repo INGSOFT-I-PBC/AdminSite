b3b5e04d1e18cfa59d14ccd90492697aa92b5483
neoterux <labfernandez2014@gmail.com>
2022-11-12 19:40:14 -0500

    [#182727477] Updated the store to fetch order requests

--- frontend/src/store/order.ts
+++ frontend/src/store/order.ts
@@ -1,12 +1,50 @@
-import { defineStore } from 'pinia'
-import type { OrderSaveData } from '@store/types/orders.model'
-import axios from 'axios'
+import type { PaginatedResponse } from '@store-types'
 import type { MessageResponse } from '@store/types'
+import type { OrderRequest, OrderSaveData } from '@store/types/orders.model'
+import axios from 'axios'
+import { defineStore } from 'pinia'
 
 export const useOrderStore = defineStore('orders', () => {
+    const order = ref<OrderRequest>()
+    const orderRequests = ref<PaginatedResponse<OrderRequest>>()
     async function saveOrder(order: OrderSaveData) {
         return axios.post<MessageResponse>('/api/v1/order', order)
     }
 
-    return { saveOrder }
+    /**
+     * This function search a specific order and return all its detailed data.
+     *
+     * @param id The id of the desired order request
+     */
+    async function fetchOrder(id: number) {
+        const data = (
+            await axios.get<OrderRequest>(`/api/v1/detailed/order/${id}`)
+        ).data
+        order.value = data
+
+        return data
+    }
+
+    /**
+     * This function returns a paginated list of Order Requests
+     * @param params the search params for this route
+     */
+    async function fetchRequests(params: PaginationOptions) {
+        const data = (
+            await axios.get<PaginatedResponse<OrderRequest>>(
+                '/api/v1/list/warehouses/order-requests',
+                { params }
+            )
+        ).data
+        orderRequests.value = data
+        return data
+    }
+
+    return {
+        orderRequests,
+        order,
+        saveOrder,
+        fetchRequests,
+        fetchOrder,
+    }
 })
