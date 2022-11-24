29cc713e1033ffd87d6198d98fc623f73c9d0501
alejandraquimi <alevquim@espol.edu.ec>
2022-11-23 20:31:34 -0500

    merge

--- api/views/inventoryviews.py
+++ api/views/inventoryviews.py
@@ -8,6 +8,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from api.models import Employee, Inventory, Item, Warehouse
 from api.serializers import FullInventorySerializer, InventorySerializer
@@ -87,6 +88,7 @@ class InventoryView(APIView):
             datos3 = {"message": "Items not found .."}
 
         return JsonResponse(datos3)
+
 class FullInventoryViewSet(ReadOnlyModelViewSet):
   
     queryset = Inventory.objects.all().order_by("-created_at")
