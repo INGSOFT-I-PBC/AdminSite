c178c9b74275ac01ba95a33c066513da9d870718
José Jaramillo <josjjara@espol.edu.ec>
2022-12-14 22:30:51 -0500

    Gestion Pediddos and bodegas fix

--- api/serializers/purchase.py
+++ api/serializers/purchase.py
@@ -3,46 +3,71 @@ from rest_framework import serializers
 from rest_framework.fields import CharField
 
 from api.models import Purchase
-from api.models.purchases import PurchaseStatus
-from api.serializers.auth import EmployeeSerializer
-from api.serializers.common import StatusSerializer
+from api.models.orders import OrderRequest
+from api.models.purchases import PurchaseChild, PurchaseDetail, PurchaseStatus
+from api.models.warehouse import Warehouse
+from api.serializers.common import SimpleStatusSerializer
 from api.serializers.inovice import SimpleInvoiceSerializer
+from api.serializers.product import SimpleProductSerializer, SimpleVariantSerializer
 from api.serializers.provider import SimpleProviderSerializer
+from api.serializers.users import EmployeeSerializer
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = "__all__"
 
+
+class PurchaseChildSerializer(serializers.ModelSerializer):
     invoice = SimpleInvoiceSerializer()
     provider = SimpleProviderSerializer()
 
     class Meta:
-        model = Purchase
-        fields = [
-            "id",
-            "aproved_at",
-            "img_details",
-            "invoice",
-            "provider",
-            "order_origin",
-            "reference",
-            "warehouse",
-        ]
+        model = PurchaseChild
+        fields = "__all__"
+
+
+class PurchaseDetailSerializer(serializers.ModelSerializer):
+
+    product = SimpleProductSerializer()
+    variant = SimpleVariantSerializer()
+
+    class Meta:
+        model = PurchaseDetail
+        fields = "__all__"
+
+
+class AttachedOrderRequestSerializer(serializers.ModelSerializer):
+
+    revised_by = EmployeeSerializer()
+
+    class Meta:
+        model = OrderRequest
+        fields = ["id", "comment", "revised_by", "requested_at"]
+
+
+class AttachedWarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = ["id", "name"]
 
 
 class PurchaseStatusSerializer(serializers.ModelSerializer):
+    """
+    Simple purchase serializers to get minimum information of a purchase with
+    the provider reference and last status registered.
+    """
 
     status = CharField()
-    invoice = SimpleInvoiceSerializer()
-    provider = SimpleProviderSerializer()
+    order_origin = AttachedOrderRequestSerializer()
+    warehouse = AttachedWarehouseSerializer()
 
     class Meta:
         model = Purchase
         fields = [
             "id",
-            "aproved_at",
-            "img_details",
-            "invoice",
-            "provider",
+            "approved_at",
             "order_origin",
             "reference",
             "warehouse",
@@ -54,10 +79,46 @@ class StatusPurchaseSerializer(serializers.ModelSerializer):
 
     id = IntegerField()
     created_at = DateTimeField()
+    created_by = EmployeeSerializer()
+    status = SimpleStatusSerializer()
 
-    def create(self, validated_data):
-        return PurchaseStatus.objects.create(**validated_data)
+    def to_representation(self, obj):
+        """Move fields from status to purchase_status representation."""
+        representation = super().to_representation(obj)
+        status_representation = representation.pop("status")
+        representation["status"] = status_representation["name"]
+        return representation
 
     class Meta:
         model = PurchaseStatus
         fields = ["id", "purchase", "status", "created_by", "created_at"]
+
+
+class SavePurchaseStatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseStatus
+        fields = ["id", "purchase", "status", "created_by", "created_at"]
+
+
+class PurchaseAditionalInfoSerialzier(serializers.ModelSerializer):
+    """
+    Purchase Serializer that contains the list of purchase_childs ids and a list of all related purchase_status.
+    """
+
+    childs = serializers.SerializerMethodField(read_only=True)
+    status_list = serializers.SerializerMethodField(read_only=True)
+    warehouse = AttachedWarehouseSerializer()
+    status = CharField()
+    order_origin = AttachedOrderRequestSerializer()
+
+    def get_childs(self, instance):
+        qs = PurchaseChild.objects.filter(purchase_header=instance)
+        return PurchaseChildSerializer(qs, many=True).data
+
+    def get_status_list(self, instance):
+        qs = PurchaseStatus.objects.filter(purchase=instance)
+        return StatusPurchaseSerializer(qs, many=True).data
+
+    class Meta:
+        model = Purchase
+        fields = "__all__"
--- api/views/purchase.py
+++ api/views/purchase.py
@@ -1,39 +1,86 @@
 from datetime import datetime, timedelta
 
 from django.db.models import OuterRef, Subquery
+from django.db.models.aggregates import Max
+from django.db.models.expressions import F
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from api.models import Purchase
+from api.models import Purchase, User
 from api.models.common import Status
-from api.models.purchases import PurchaseStatus
-from api.serializers.purchase import PurchaseStatusSerializer, StatusPurchaseSerializer
+from api.models.purchases import PurchaseChild, PurchaseDetail, PurchaseStatus
+from api.serializers.purchase import (
+    PurchaseAditionalInfoSerialzier,
+    PurchaseDetailSerializer,
+    PurchaseStatusSerializer,
+    SavePurchaseStatusSerializer,
+)
 from api.utils import error_response
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 1000
+    page_size_query_param = "per_page"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "data": data,
+                "lastPage": self.page.paginator.num_pages,
+                "page": self.page.number,
+                "total": self.page.paginator.count,
+            }
+        )
 
 
 class PurchaseViewSet(ModelViewSet):
     """
-    API endpoint that allows purchases to be viewed or edited.
+    API endpoint that allows purchases to be viewed.
     """
 
-    queryset = Purchase.objects.all().order_by("-aproved_at")
+    queryset = Purchase.objects.all().order_by("-approved_at")
     serializer_class = PurchaseStatusSerializer
 
     def get_queryset(self):
-        queryset = self.queryset
+        queryset = self.queryset.select_related("order_origin")
         params = self.request.query_params.copy()
+
+        queryset = queryset.annotate(
+            status=Subquery(
+                PurchaseStatus.objects.filter(purchase=OuterRef("id"))
+                .order_by("-created_at")
+                .select_related("status")
+                .values("status__name")[:1]
+            )
+        )
 
         if params.get("order_by", None):
             fields = params.pop("order_by")
             queryset = queryset.order_by(*fields)
 
+        if params.get("purchase_id", None):
+            queryset = queryset.filter(id=params.get("purchase_id"))
+            return queryset
+
         if params.get("reference", None):
             queryset = queryset.filter(reference=params.get("reference"))
+            return queryset
 
         if params.get("invoice_id", None):
-            queryset = queryset.filter(invoice=params.get("invoice_id"))
+            queryset = queryset.filter(
+                id__in=Subquery(
+                    PurchaseChild.objects.filter(
+                        invoice__id=(params.get("invoice_id").values("purchase_header"))
+                    )
+                )
+            )
+            return queryset
+
+        if params.get("order_id", None):
+            queryset = queryset.filter(order_origin__id=params.get("order_id"))
             return queryset
 
         if params.get("invoice_code", None):
@@ -44,7 +91,7 @@ class PurchaseViewSet(ModelViewSet):
         if params.get("warehouse_id", None):
             queryset = queryset.filter(
                 warehouse=params.get("warehouse_id")
-            ).prefetch_related("warehouse", "provider", "invoice")
+            ).prefetch_related("warehouse")
 
         if params.get("warehouse_name", None):
             queryset = queryset.filter(
@@ -52,26 +99,53 @@ class PurchaseViewSet(ModelViewSet):
             )
 
         if params.get("status", None):
-            purchase_status = PurchaseStatus.objects.filter(
-                status__name__icontains=params.get("status")
-            ).order_by("-created_at")
 
-            queryset = queryset.filter(
-                id=Subquery(purchase_status.values("purchase")[:1])
+            p_status = (
+                PurchaseStatus.objects.annotate(
+                    last_status_pk=Max("purchase__purchasestatus__pk")
+                )
+                .filter(pk=F("last_status_pk"))
+                .filter(status__name__icontains=params.get("status"))
             )
+
+            if params.get("status_from_date", None):
+                p_status = p_status.filter(
+                    created_at__gte=params.get("status_from_date")
+                )
+
+            if params.get("status_to_date", None):
+                to_date = datetime.strptime(
+                    (params.get("status_to_date")), "%Y-%m-%dT%H:%M:%S.%f%z"
+                ) + timedelta(days=1)
+
+                p_status = p_status.filter(created_at__gte=to_date)
+
+            queryset = queryset.filter(pk__in=Subquery(p_status.values("purchase")))
 
         if params.get("provider_name", None):
             queryset = queryset.filter(
-                provider__name__icontains=params.get("provider_name")
+                id__in=Subquery(
+                    PurchaseChild.objects.filter(
+                        provider__name__icontains=params.get("provider_header").values(
+                            "purchase"
+                        )
+                    )
+                )
             )
 
         if params.get("from_date", None):
             if params.get("invoice_date", None):
                 queryset = queryset.filter(
-                    invoice__created_at__gte=(params["from_date"])
+                    id__in=Subquery(
+                        PurchaseChild.objects.filter(
+                            invoice__created_at__gte=(
+                                params.get("from_date").values("purchase_header")
+                            )
+                        )
+                    )
                 )
-            if params.get("aproved_date", None):
-                queryset = queryset.filter(aproved_at__gte=(params["from_date"]))
+            if params.get("approved_date", None):
+                queryset = queryset.filter(approved_at__gte=(params["from_date"]))
 
         if params.get("to_date", None):
             to_date = datetime.strptime(
@@ -79,23 +153,83 @@ class PurchaseViewSet(ModelViewSet):
             ) + timedelta(days=1)
 
             if params.get("invoice_date", None):
-                queryset = queryset.filter(invoice__created_at__lte=to_date)
-            if params.get("aproved_date", None):
-                queryset = queryset.filter(aproved_at__lte=to_date)
+                queryset = queryset.filter(
+                    id__in=Subquery(
+                        PurchaseChild.objects.filter(
+                            invoice__created_at__lte=(
+                                params.get("from_date").values("purchase_header")
+                            )
+                        )
+                    )
+                )
+            if params.get("approved_date", None):
+                queryset = queryset.filter(approved_at__lte=to_date)
 
-        queryset = queryset.annotate(
-            status=Subquery(
-                PurchaseStatus.objects.filter(purchase=OuterRef("id"))
-                .order_by("-created_at")
-                .values("status__name")[:1]
+        return queryset
+
+
+class PurchaseAditionalInfoViewSet(PurchaseViewSet):
+    """Inherits PurchaseViewSet
+    Provides more details of the purchase after filtering
+    """
+
+    serializer_class = PurchaseAditionalInfoSerialzier
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset.select_related("warehouse")
+
+        return queryset
+
+
+class PurchaseDetailsViewSet(ModelViewSet):
+
+    queryset = PurchaseDetail.objects.all().order_by("-id")
+    serializer_class = PurchaseDetailSerializer
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = self.queryset
+        params = self.request.query_params.copy()
+
+        if params.get("order_by", None):
+            fields = params.pop("order_by")
+            queryset = queryset.order_by(*fields)
+
+        if (
+            params.get("purchase_id", None)
+            and params.get("invoice_id", None)
+            and params.get("purchase_child_id", None)
+        ):
+            return error_response("Invalid request")
+
+        if params.get("purchase_id", None):
+            purchase = Purchase.objects.get(
+                id=params.get("purchase_id")
+            ).prefetch_realted("purchase_child")
+            queryset = queryset.filter(
+                purchase_child__in=Subquery(
+                    PurchaseChild.objects.filter(purchase=purchase)
+                )
             )
-        )
+        if params.get("purchase_child_id", None):
+            queryset = queryset.filter(purchase_child=params.get("purchase_child_id"))
+            return queryset
 
         return queryset
 
 
 @api_view(["POST"])
-def confirm_purchase(request):
+def create_purchase_status(request):
+    """Creates a status for the Purchase Model
+
+    Args:
+        request: Post request
+
+    Returns:
+       Success: The purchase_status object data, http status code 201
+       Failure: The Json error response with error code 400
+    """
     if not request.data:
         return error_response("No data was provided")
 
@@ -103,19 +237,33 @@ def confirm_purchase(request):
         return error_response("Incomplete data")
 
     id = int(request.data.get("id"))
-    purchase = Purchase.objects.filter(pk=id).first()
+    purchase = Purchase.objects.get(pk=id)
     status_obj = Status.objects.filter(name=request.data.get("status")).first()
+    employee = User.objects.get(id=request.user.id).employee
 
+    print(employee)
     data = {
+        "created_at": datetime.now(),
+        "created_by": employee.id,
         "purchase": purchase.id,
         "status": status_obj.id,
-        "created_by": request.user.id,
-        "created_at": datetime.now(),
     }
+    print(data)
 
-    serializer = StatusPurchaseSerializer(data=data)
+    serializer = SavePurchaseStatusSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        if request.data.get("status") == "entregado":
+            purchase_childs = PurchaseChild.objects.filter(
+                purchase_header=request.data.get("id")
+            )
+            purchase_childs.update(is_delivered=True)
 
-    if serializer.is_valid():
+        if request.data.get("status") == "pagado":
+            purchase_childs = PurchaseChild.objects.filter(
+                purchase_header=request.data.get("id")
+            )
+            purchase_childs.update(is_purchased=True)
+
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return error_response("Invalid data")
