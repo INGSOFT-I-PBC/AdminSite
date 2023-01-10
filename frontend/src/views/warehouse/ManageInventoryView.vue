6e66f1758dcc79dffc27aa8c62a86193ee8b63b2
José Jaramillo <josjjara@espol.edu.ec>
2023-01-09 19:34:15 -0500

    drawer config

--- frontend/src/views/warehouse/ManageInventoryView.vue
+++ frontend/src/views/warehouse/ManageInventoryView.vue
@@ -1,56 +1,105 @@
-<template>
-    <ECard>
-        <Title size="2xl">Gestión de inventario</Title>
-        <div
-            class="tw-h-0.5 tw-w-full tw-bg-secondary dark:tw-bg-primary"></div>
-        <WaitOverlay :show="loadingState.waitWarehouse">
-            <div class="container">
-                <div class="row">
-                    <!-- left division -->
-                    <div
-                        class="col col-3 tw-bg-slate-600 tw-py-2 tw-rounded-lg tw-text-white">
-                        <VeeForm>
-                            <VeeField
-                                label="Hola"
-                                name="name"
-                                v-slot="{ field, handleChange }">
-                                <InputText
-                                    label="Búsqueda"
-                                    :model-value="field.value as string"
-                                    @update:model-value="handleChange" />
-                            </VeeField>
-                        </VeeForm>
-                        <BPagination
-                            :total-rows="(warehouseStore.paginatedWarehouse as PaginatedResponse<Warehouse>)?.total"
-                            :per-page="paginationData.per_page" />
-                    </div>
-
-                    <!-- right division, must contain all the information about the warehouse -->
-                    <div class="col">
-                        <WaitOverlay :show="loadingState.waitData">
-                        </WaitOverlay>
-                    </div>
-                </div>
-            </div>
-        </WaitOverlay>
-    </ECard>
-</template>
-
 <script setup lang="ts">
     import { useWarehouseStore } from '@store'
-    import type { PaginatedResponse, Warehouse } from '@store/types'
+    import type { Warehouse } from '@store/types'
     import { BPagination } from 'bootstrap-vue-3'
 
-    import { ECard, InputText, Title, WaitOverlay } from '@custom-components'
+    import { ref, watch } from 'vue'
+
+    import { ECard, WaitOverlay } from '@custom-components'
+
+    const warehouseStore = useWarehouseStore()
 
     const loadingState = ref({
         waitWarehouse: false,
         waitData: false,
     })
 
-    const paginationData = ref<PaginationOptions>({
-        page: 1,
-        per_page: 10,
-    })
-    const warehouseStore = useWarehouseStore()
+    const toggle = ref<boolean>(true)
+
+    const currentAsidePage = ref<number>(1)
+    const whRows = ref<number>(0)
+    const paginatedWarehouse = ref<Warehouse[]>()
+    const whPageCount = ref<number>(15)
+    const activeWharehouseButton = ref<number>(-1)
+    const selectedWarehouse = ref<Warehouse>()
+
+    function paginateAside() {
+        paginatedWarehouse.value = (
+            warehouseStore.getWarehouseList ?? []
+        ).slice(
+            (currentAsidePage.value - 1) * whPageCount.value,
+            currentAsidePage.value * whPageCount.value
+        )
+    }
+
+    function wharehouseButtonPressed(wh: Warehouse, index: number) {
+        selectedWarehouse.value = wh
+    }
+
+    warehouseStore.fetchWarehouses()
+
+    watch(currentAsidePage, paginateAside)
 </script>
+
+<template>
+    <WaitOverlay :show="loadingState.waitWarehouse">
+        <ECard>
+            <div
+                class="row d-inline-flex allign-content-center tw-bg-slate-50 dark:tw-bg-slate-600">
+                <div
+                    v-if="toggle"
+                    class="col-sm-12 col-md-12 col-xl-2 mx-1 tw-flex-col tw-rounded-lg tw-bg-white dark:tw-bg-slate-800 justify-center">
+                    <h1 class="title my-2">Bodegas Disponibles</h1>
+
+                    <e-button
+                        @click.left="
+                            () => {
+                                toggle = false
+                            }
+                        "
+                        class="tw-text-slate-800 col-lg-3 col-md-3 col-sm-6 col-xl-6 mx-3 mb-3"
+                        left-icon="chevrons-left"
+                        icon-provider="feather">
+                    </e-button>
+
+                    <b-pagination
+                        v-model="currentAsidePage"
+                        :total-rows="whRows"
+                        :per-page="whPageCount"
+                        aria-controls="b-list-warehouses"
+                        :limit="3"
+                        hide-goto-end-buttons
+                        class="paginator mb-2"></b-pagination>
+
+                    <b-list-group
+                        :per-page="whPageCount"
+                        :current-page="currentAsidePage"
+                        class="button-group smaller-btn-group col-lg-12 col-md-6 col-sm-6">
+                        <b-list-group-item
+                            :class="{ active: activeWharehouseButton === -1 }"
+                            button
+                            @click.left="
+                                () => {
+                                    activeWharehouseButton = -1
+                                }
+                            ">
+                            Todas las bodegas
+                        </b-list-group-item>
+                        <b-list-group-item
+                            v-for="(wh, index) in paginatedWarehouse"
+                            button
+                            :class="{
+                                active: wh.id === activeWharehouseButton,
+                            }"
+                            :key="wh.id"
+                            :id="'whbtn-' + wh.id"
+                            @click.left="wharehouseButtonPressed(wh, index)">
+                            {{ wh.name }}
+                        </b-list-group-item>
+                    </b-list-group>
+                </div>
+                <div class="col-xl tw-bg-slate-50 dark:tw-bg-slate-700"></div>
+            </div>
+        </ECard>
+    </WaitOverlay>
+</template>
