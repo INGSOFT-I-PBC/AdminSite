45d60195c0f699db0947744a6d20b6307a0378fa
José Jaramillo <josjjara@espol.edu.ec>
2023-01-17 08:12:36 -0500

    fix

--- frontend/src/store/types/items.model.d.ts
+++ frontend/src/store/types/items.model.d.ts
@@ -39,48 +39,6 @@ export type ItemValue = {
     base_price?: string
 }
 
-export interface ProductProvider {
-    active?: boolean
-    price: string
-    product: number
-    provider: number
-}
-
-export type ProductValues = {
-    id?: number
-    created_at?: string
-    updated_at?: string
-    deleted_at?: string
-    variant_name?: string
-    sku?: string
-    price?: string
-    is_active?: boolean
-    prod_id?: number
-    product_name?: string
-    brand_name?: string
-    sku?: string
-    summary?: string
-    short_description?: string
-    base_price?: string
-}
-
-export type ProviderValues = {
-    name?: string
-    document_path?: string
-    bussiness_name?: string
-    address?: string
-    website?: string
-    email?: string
-    latitude?: string
-    longitude?: string
-}
-
-export interface ProviderProductDetails {
-    product?: ProductValues[]
-    provider?: ProviderValues[]
-    price?: string
-}
-
 export interface FullItem extends Item {
     meta: Optional<MetaData>
 }
@@ -89,23 +47,6 @@ export interface ItemProps {
     name: string
     type: string
     value: Optional<string>
-}
-
-export type ProductsPurchase = {
-    item?: number
-    quantity?: number
-    price?: number
-}
-
-export interface DetailsOrders {
-    provider?: number
-    products?: ProductsPurchase[]
-}
-
-export interface PurchaseOrder extends DetailsOrders {
-    warehouse_id?: number
-    order_origin?: number
-    details?: DetailsOrders[]
 }
 
 export default {}
